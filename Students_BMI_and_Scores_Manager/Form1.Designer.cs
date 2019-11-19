f6273aac741ca9dad870954686c455f9dffe3190
郵差 <a34007024@gmail.com>
2019-11-19 10:04:28 +0800

    add basic Layout and Simple functions

--- Students_BMI_and_Scores_Manager/Form1.Designer.cs
+++ Students_BMI_and_Scores_Manager/Form1.Designer.cs
@@ -28,13 +28,202 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameTextbox = new System.Windows.Forms.TextBox();
+            this.heightTextbox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.idLabel = new System.Windows.Forms.Label();
+            this.weightTextbox = new System.Windows.Forms.TextBox();
+            this.addDataBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(12, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "姓名";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(12, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(165, 40);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "身高(公尺)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微軟正黑體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(12, 248);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(165, 40);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "體重(公斤)";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dataGridView1.Location = new System.Drawing.Point(379, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 31;
+            this.dataGridView1.Size = new System.Drawing.Size(867, 520);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "編號(ID)";
+            this.Column1.MinimumWidth = 8;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 150;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "姓名";
+            this.Column2.MinimumWidth = 8;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 150;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "身高";
+            this.Column3.MinimumWidth = 8;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 150;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "體重";
+            this.Column4.MinimumWidth = 8;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 150;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "BMI";
+            this.Column5.MinimumWidth = 8;
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 150;
+            // 
+            // nameTextbox
+            // 
+            this.nameTextbox.Font = new System.Drawing.Font("微軟正黑體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.nameTextbox.Location = new System.Drawing.Point(203, 105);
+            this.nameTextbox.Name = "nameTextbox";
+            this.nameTextbox.Size = new System.Drawing.Size(150, 50);
+            this.nameTextbox.TabIndex = 4;
+            // 
+            // heightTextbox
+            // 
+            this.heightTextbox.Font = new System.Drawing.Font("微軟正黑體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.heightTextbox.Location = new System.Drawing.Point(203, 176);
+            this.heightTextbox.Name = "heightTextbox";
+            this.heightTextbox.Size = new System.Drawing.Size(150, 50);
+            this.heightTextbox.TabIndex = 5;
+            this.heightTextbox.TextChanged += new System.EventHandler(this.heightTextbox_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微軟正黑體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(12, 38);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(134, 40);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "編號(ID)";
+            // 
+            // idLabel
+            // 
+            this.idLabel.AutoSize = true;
+            this.idLabel.Font = new System.Drawing.Font("微軟正黑體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.idLabel.Location = new System.Drawing.Point(217, 38);
+            this.idLabel.Name = "idLabel";
+            this.idLabel.Size = new System.Drawing.Size(125, 40);
+            this.idLabel.TabIndex = 7;
+            this.idLabel.Text = "idLabel";
+            // 
+            // weightTextbox
+            // 
+            this.weightTextbox.Font = new System.Drawing.Font("微軟正黑體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.weightTextbox.Location = new System.Drawing.Point(203, 245);
+            this.weightTextbox.Name = "weightTextbox";
+            this.weightTextbox.Size = new System.Drawing.Size(150, 50);
+            this.weightTextbox.TabIndex = 8;
+            this.weightTextbox.TextChanged += new System.EventHandler(this.weightTextbox_TextChanged);
+            // 
+            // addDataBtn
+            // 
+            this.addDataBtn.Font = new System.Drawing.Font("微軟正黑體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.addDataBtn.Location = new System.Drawing.Point(65, 343);
+            this.addDataBtn.Name = "addDataBtn";
+            this.addDataBtn.Size = new System.Drawing.Size(248, 63);
+            this.addDataBtn.TabIndex = 9;
+            this.addDataBtn.Text = "新增資料";
+            this.addDataBtn.UseVisualStyleBackColor = true;
+            this.addDataBtn.Click += new System.EventHandler(this.addDataBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1258, 664);
+            this.Controls.Add(this.addDataBtn);
+            this.Controls.Add(this.weightTextbox);
+            this.Controls.Add(this.idLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.heightTextbox);
+            this.Controls.Add(this.nameTextbox);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "北護學生BMI與成績管理分析系統";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox nameTextbox;
+        private System.Windows.Forms.TextBox heightTextbox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label idLabel;
+        private System.Windows.Forms.TextBox weightTextbox;
+        private System.Windows.Forms.Button addDataBtn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
     }
 }
 
--- Students_BMI_and_Scores_Manager/Form1.cs
+++ Students_BMI_and_Scores_Manager/Form1.cs
@@ -12,9 +12,32 @@ namespace Students_BMI_and_Scores_Manager
 {
     public partial class Form1 : Form
     {
+        int index = 1;
+        double height;
+        double weight;
+        string name;
         public Form1()
         {
             InitializeComponent();
+            idLabel.Text = index.ToString();
+        }
+        
+        private void addDataBtn_Click(object sender, EventArgs e)
+        {
+            height = double.Parse(heightTextbox.Text);
+            weight = double.Parse(weightTextbox.Text);
+            name = nameTextbox.Text;
+            dataGridView1.Rows.Add(new object[] { index, name, height, weight});
+        }
+
+        private void heightTextbox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void weightTextbox_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }

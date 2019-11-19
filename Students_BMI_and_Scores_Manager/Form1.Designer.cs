d0723c2bd8496c9d89027f8f99a3080180442279
郵差 <a34007024@gmail.com>
2019-11-19 11:28:06 +0800

    add new textbox and label
    國英數成績的輸入文字框

--- Students_BMI_and_Scores_Manager/Form1.Designer.cs
+++ Students_BMI_and_Scores_Manager/Form1.Designer.cs
@@ -33,11 +33,6 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.nameTextbox = new System.Windows.Forms.TextBox();
             this.heightTextbox = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
@@ -45,6 +40,20 @@
             this.weightTextbox = new System.Windows.Forms.TextBox();
             this.addDataBtn = new System.Windows.Forms.Button();
             this.editDataBtn = new System.Windows.Forms.Button();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mathScoreTextbox = new System.Windows.Forms.TextBox();
+            this.englishScoreTextbox = new System.Windows.Forms.TextBox();
+            this.chineseScoreTextbox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -52,7 +61,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("微軟正黑體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label1.Location = new System.Drawing.Point(12, 108);
+            this.label1.Location = new System.Drawing.Point(9, 82);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(81, 40);
             this.label1.TabIndex = 0;
@@ -62,7 +71,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("微軟正黑體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label2.Location = new System.Drawing.Point(12, 179);
+            this.label2.Location = new System.Drawing.Point(9, 153);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(165, 40);
             this.label2.TabIndex = 1;
@@ -72,7 +81,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("微軟正黑體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label3.Location = new System.Drawing.Point(12, 248);
+            this.label3.Location = new System.Drawing.Point(9, 222);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(165, 40);
             this.label3.TabIndex = 2;
@@ -89,7 +98,10 @@
             this.Column2,
             this.Column3,
             this.Column4,
-            this.Column5});
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8});
             this.dataGridView1.Location = new System.Drawing.Point(379, 12);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 62;
@@ -97,6 +109,76 @@
             this.dataGridView1.Size = new System.Drawing.Size(867, 520);
             this.dataGridView1.TabIndex = 3;
             this.dataGridView1.RowHeaderMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.double_click);
+            // 
+            // nameTextbox
+            // 
+            this.nameTextbox.Font = new System.Drawing.Font("微軟正黑體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.nameTextbox.Location = new System.Drawing.Point(200, 79);
+            this.nameTextbox.Name = "nameTextbox";
+            this.nameTextbox.Size = new System.Drawing.Size(150, 50);
+            this.nameTextbox.TabIndex = 4;
+            // 
+            // heightTextbox
+            // 
+            this.heightTextbox.Font = new System.Drawing.Font("微軟正黑體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.heightTextbox.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.heightTextbox.Location = new System.Drawing.Point(200, 150);
+            this.heightTextbox.Name = "heightTextbox";
+            this.heightTextbox.Size = new System.Drawing.Size(150, 50);
+            this.heightTextbox.TabIndex = 5;
+            this.heightTextbox.TextChanged += new System.EventHandler(this.heightTextbox_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微軟正黑體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(9, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(134, 40);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "編號(ID)";
+            // 
+            // idLabel
+            // 
+            this.idLabel.AutoSize = true;
+            this.idLabel.Font = new System.Drawing.Font("微軟正黑體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.idLabel.Location = new System.Drawing.Point(214, 12);
+            this.idLabel.Name = "idLabel";
+            this.idLabel.Size = new System.Drawing.Size(125, 40);
+            this.idLabel.TabIndex = 7;
+            this.idLabel.Text = "idLabel";
+            // 
+            // weightTextbox
+            // 
+            this.weightTextbox.Font = new System.Drawing.Font("微軟正黑體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.weightTextbox.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.weightTextbox.Location = new System.Drawing.Point(200, 219);
+            this.weightTextbox.Name = "weightTextbox";
+            this.weightTextbox.Size = new System.Drawing.Size(150, 50);
+            this.weightTextbox.TabIndex = 8;
+            this.weightTextbox.TextChanged += new System.EventHandler(this.weightTextbox_TextChanged);
+            // 
+            // addDataBtn
+            // 
+            this.addDataBtn.Font = new System.Drawing.Font("微軟正黑體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.addDataBtn.Location = new System.Drawing.Point(65, 520);
+            this.addDataBtn.Name = "addDataBtn";
+            this.addDataBtn.Size = new System.Drawing.Size(248, 63);
+            this.addDataBtn.TabIndex = 9;
+            this.addDataBtn.Text = "新增資料";
+            this.addDataBtn.UseVisualStyleBackColor = true;
+            this.addDataBtn.Click += new System.EventHandler(this.addDataBtn_Click);
+            // 
+            // editDataBtn
+            // 
+            this.editDataBtn.Font = new System.Drawing.Font("微軟正黑體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.editDataBtn.Location = new System.Drawing.Point(65, 589);
+            this.editDataBtn.Name = "editDataBtn";
+            this.editDataBtn.Size = new System.Drawing.Size(248, 63);
+            this.editDataBtn.TabIndex = 10;
+            this.editDataBtn.Text = "修改資料";
+            this.editDataBtn.UseVisualStyleBackColor = true;
+            this.editDataBtn.Click += new System.EventHandler(this.editDataBtn_Click);
             // 
             // Column1
             // 
@@ -133,81 +215,95 @@
             this.Column5.Name = "Column5";
             this.Column5.Width = 150;
             // 
-            // nameTextbox
+            // Column6
             // 
-            this.nameTextbox.Font = new System.Drawing.Font("微軟正黑體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.nameTextbox.Location = new System.Drawing.Point(203, 105);
-            this.nameTextbox.Name = "nameTextbox";
-            this.nameTextbox.Size = new System.Drawing.Size(150, 50);
-            this.nameTextbox.TabIndex = 4;
+            this.Column6.HeaderText = "國文成績";
+            this.Column6.MinimumWidth = 8;
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 150;
             // 
-            // heightTextbox
+            // Column7
             // 
-            this.heightTextbox.Font = new System.Drawing.Font("微軟正黑體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.heightTextbox.ImeMode = System.Windows.Forms.ImeMode.Disable;
-            this.heightTextbox.Location = new System.Drawing.Point(203, 176);
-            this.heightTextbox.Name = "heightTextbox";
-            this.heightTextbox.Size = new System.Drawing.Size(150, 50);
-            this.heightTextbox.TabIndex = 5;
-            this.heightTextbox.TextChanged += new System.EventHandler(this.heightTextbox_TextChanged);
+            this.Column7.HeaderText = "英文成績";
+            this.Column7.MinimumWidth = 8;
+            this.Column7.Name = "Column7";
+            this.Column7.Width = 150;
             // 
-            // label4
+            // Column8
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("微軟正黑體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label4.Location = new System.Drawing.Point(12, 38);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(134, 40);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "編號(ID)";
+            this.Column8.HeaderText = "數學成績";
+            this.Column8.MinimumWidth = 8;
+            this.Column8.Name = "Column8";
+            this.Column8.Width = 150;
             // 
-            // idLabel
+            // mathScoreTextbox
             // 
-            this.idLabel.AutoSize = true;
-            this.idLabel.Font = new System.Drawing.Font("微軟正黑體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.idLabel.Location = new System.Drawing.Point(217, 38);
-            this.idLabel.Name = "idLabel";
-            this.idLabel.Size = new System.Drawing.Size(125, 40);
-            this.idLabel.TabIndex = 7;
-            this.idLabel.Text = "idLabel";
+            this.mathScoreTextbox.Font = new System.Drawing.Font("微軟正黑體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.mathScoreTextbox.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.mathScoreTextbox.Location = new System.Drawing.Point(200, 431);
+            this.mathScoreTextbox.Name = "mathScoreTextbox";
+            this.mathScoreTextbox.Size = new System.Drawing.Size(150, 50);
+            this.mathScoreTextbox.TabIndex = 16;
             // 
-            // weightTextbox
+            // englishScoreTextbox
             // 
-            this.weightTextbox.Font = new System.Drawing.Font("微軟正黑體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.weightTextbox.ImeMode = System.Windows.Forms.ImeMode.Disable;
-            this.weightTextbox.Location = new System.Drawing.Point(203, 245);
-            this.weightTextbox.Name = "weightTextbox";
-            this.weightTextbox.Size = new System.Drawing.Size(150, 50);
-            this.weightTextbox.TabIndex = 8;
-            this.weightTextbox.TextChanged += new System.EventHandler(this.weightTextbox_TextChanged);
+            this.englishScoreTextbox.Font = new System.Drawing.Font("微軟正黑體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.englishScoreTextbox.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.englishScoreTextbox.Location = new System.Drawing.Point(200, 362);
+            this.englishScoreTextbox.Name = "englishScoreTextbox";
+            this.englishScoreTextbox.Size = new System.Drawing.Size(150, 50);
+            this.englishScoreTextbox.TabIndex = 15;
             // 
-            // addDataBtn
+            // chineseScoreTextbox
             // 
-            this.addDataBtn.Font = new System.Drawing.Font("微軟正黑體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.addDataBtn.Location = new System.Drawing.Point(65, 343);
-            this.addDataBtn.Name = "addDataBtn";
-            this.addDataBtn.Size = new System.Drawing.Size(248, 63);
-            this.addDataBtn.TabIndex = 9;
-            this.addDataBtn.Text = "新增資料";
-            this.addDataBtn.UseVisualStyleBackColor = true;
-            this.addDataBtn.Click += new System.EventHandler(this.addDataBtn_Click);
+            this.chineseScoreTextbox.Font = new System.Drawing.Font("微軟正黑體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.chineseScoreTextbox.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.chineseScoreTextbox.Location = new System.Drawing.Point(200, 291);
+            this.chineseScoreTextbox.Name = "chineseScoreTextbox";
+            this.chineseScoreTextbox.Size = new System.Drawing.Size(150, 50);
+            this.chineseScoreTextbox.TabIndex = 14;
             // 
-            // editDataBtn
+            // label5
             // 
-            this.editDataBtn.Font = new System.Drawing.Font("微軟正黑體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.editDataBtn.Location = new System.Drawing.Point(65, 438);
-            this.editDataBtn.Name = "editDataBtn";
-            this.editDataBtn.Size = new System.Drawing.Size(248, 63);
-            this.editDataBtn.TabIndex = 10;
-            this.editDataBtn.Text = "修改資料";
-            this.editDataBtn.UseVisualStyleBackColor = true;
-            this.editDataBtn.Click += new System.EventHandler(this.editDataBtn_Click);
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("微軟正黑體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.Location = new System.Drawing.Point(9, 434);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(145, 40);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "數學成績";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("微軟正黑體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label6.Location = new System.Drawing.Point(9, 365);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(145, 40);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "英文成績";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("微軟正黑體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label7.Location = new System.Drawing.Point(9, 294);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(145, 40);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "國文成績";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1258, 664);
+            this.Controls.Add(this.mathScoreTextbox);
+            this.Controls.Add(this.englishScoreTextbox);
+            this.Controls.Add(this.chineseScoreTextbox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.editDataBtn);
             this.Controls.Add(this.addDataBtn);
             this.Controls.Add(this.weightTextbox);
@@ -240,12 +336,21 @@
         private System.Windows.Forms.Label idLabel;
         private System.Windows.Forms.TextBox weightTextbox;
         private System.Windows.Forms.Button addDataBtn;
+        private System.Windows.Forms.Button editDataBtn;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
-        private System.Windows.Forms.Button editDataBtn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.TextBox mathScoreTextbox;
+        private System.Windows.Forms.TextBox englishScoreTextbox;
+        private System.Windows.Forms.TextBox chineseScoreTextbox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
     }
 }
 

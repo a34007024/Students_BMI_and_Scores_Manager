f031425c248f557842b5d8a401ae8b9761d1d4ef
郵差 <a34007024@gmail.com>
2019-12-23 11:27:24 +0800

    add new tabPage for the upcoming chart

--- Students_BMI_and_Scores_Manager/Form1.Designer.cs
+++ Students_BMI_and_Scores_Manager/Form1.Designer.cs
@@ -71,6 +71,7 @@
             this.waitingPictureBox = new System.Windows.Forms.PictureBox();
             this.waitingLabel = new System.Windows.Forms.Label();
             this.waitingTimer = new System.Windows.Forms.Timer(this.components);
+            this.tabPage3 = new System.Windows.Forms.TabPage();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
@@ -333,6 +334,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
             this.tabControl1.Location = new System.Drawing.Point(367, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
@@ -358,7 +360,7 @@
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage2.Size = new System.Drawing.Size(871, 470);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Chart";
+            this.tabPage2.Text = "Chart1";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // chart1
@@ -472,6 +474,16 @@
             this.waitingTimer.Interval = 1000;
             this.waitingTimer.Tick += new System.EventHandler(this.waitingTimer_Tick);
             // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 28);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(871, 470);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Chart2";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
@@ -559,6 +571,7 @@
         private System.Windows.Forms.PictureBox waitingPictureBox;
         private System.Windows.Forms.Label waitingLabel;
         private System.Windows.Forms.Timer waitingTimer;
+        private System.Windows.Forms.TabPage tabPage3;
     }
 }
 
